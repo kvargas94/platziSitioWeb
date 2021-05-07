778b7f506d17ff660128575a2939ea6a8c566e47
Luis Esteban Vargas Moreno <esteban.vargasmoreno@gmail.com>
2021-05-07 16:07:29 -0500

    Proyecto incial 2

---
+++ WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace platziSitioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
